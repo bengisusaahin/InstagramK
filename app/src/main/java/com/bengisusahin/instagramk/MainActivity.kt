ea221c4f7c283aebe1657589e358fd65a692c1f9
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-12-11 00:42:59 +0300

    Sign Up Operations

--- app/src/main/java/com/bengisusahin/instagramk/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/instagramk/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.bengisusahin.instagramk
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.bengisusahin.instagramk.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
     }
 
@@ -20,6 +29,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun signUpClicked(view: View){
+        val email = binding.emailText.text.toString()
+        val password = binding.passwordlText.text.toString()
 
+//        if (email.isNotEmpty() && password.isNotEmpty()){
+//
+//        }
+        if (email.equals("") || password.equals("")){
+            Toast.makeText(this, "Enter email and password!", Toast.LENGTH_LONG).show()
+        }else{
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener {
+                //success
+                val intent = Intent(this@MainActivity,FeedActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(this@MainActivity, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
 }
