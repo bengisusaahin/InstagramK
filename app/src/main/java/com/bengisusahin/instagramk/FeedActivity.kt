880c85408e94eebb1be7af4382778c3ef282b8f2
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-12-11 21:30:35 +0300

    Sign out

--- app/src/main/java/com/bengisusahin/instagramk/FeedActivity.kt
+++ app/src/main/java/com/bengisusahin/instagramk/FeedActivity.kt
@@ -1,17 +1,43 @@
 package com.bengisusahin.instagramk
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.bengisusahin.instagramk.databinding.ActivityFeedBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class FeedActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityFeedBinding
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFeedBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.inta_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_post){
+            val intent = Intent(this, UploadActivity::class.java)
+            startActivity(intent)
+        }else if (item.itemId == R.id.signout){
+            auth.signOut()
+            val intent = Intent(this, MainActivity::class.java )
+            startActivity(intent)
+            finish() //cikis yaptiktan sonra feede geri donememeli destroy etmeliyiz
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
