df2a60d92bf33cec207c091ff977dadf3833e776
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-12-13 23:49:14 +0300

    get datas

--- app/src/main/java/com/bengisusahin/instagramk/FeedActivity.kt
+++ app/src/main/java/com/bengisusahin/instagramk/FeedActivity.kt
@@ -5,23 +5,60 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.bengisusahin.instagramk.databinding.ActivityFeedBinding
+import com.bengisusahin.instagramk.model.Post
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class FeedActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityFeedBinding
     private lateinit var auth : FirebaseAuth
+    private lateinit var db : FirebaseFirestore
+    private lateinit var postArrayList : ArrayList<Post>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFeedBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         auth = Firebase.auth
+        db = Firebase.firestore
+
+        postArrayList = ArrayList<Post>()
+        getData()
     }
 
+    private fun getData(){
+
+        db.collection("Posts").addSnapshotListener { value, error ->
+            if (error != null){
+                Toast.makeText(this, error.localizedMessage, Toast.LENGTH_LONG).show()
+            }else{
+                if (value != null){
+                    if (!value.isEmpty){
+                        //value null degil ama valuenın ici bos olabiliyor
+                        val documents = value.documents
+                        for (document in documents){
+                            //casting
+                            val comment = document.get("comment") as String
+                            val userEmail = document.get("userEmail") as String
+                            val downloadUrl = document.get("downloadUrl") as String
+
+                            //println(comment)
+
+                            val post = Post(userEmail, comment, downloadUrl)
+                            postArrayList.add(post)
+                        }
+                    }
+                }
+            }
+
+        }
+    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val menuInflater = menuInflater
         menuInflater.inflate(R.menu.inta_menu, menu)
